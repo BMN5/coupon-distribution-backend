da707f41dca97ef473f058d486b4582c5a6c5a80
Manjunadh bhavaraju <161492906+BMN5@users.noreply.github.com>
2025-03-14 15:33:38 +0530

    Update ReactForwardingController.java

--- src/main/java/com/example/coupon/controller/ReactForwardingController.java
+++ src/main/java/com/example/coupon/controller/ReactForwardingController.java
@@ -1,10 +1,11 @@
+package com.example.coupon.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReactForwardingController {
-
-    @GetMapping({"/", "/add-coupon", "/claim-coupon"})
+    @GetMapping("/{path:[^\\.]*}")
     public String forwardToReact() {
         return "forward:/index.html";
     }
